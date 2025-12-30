292f6ba7982a455ac09c2f01e8bffcee41e1ce29
Ngo Tran Xuan Hoa <ngotranxuanhoa09062004@gmail.com>
2025-12-30 16:18:16 +0700

    First push with refactor code structure for improved readability and maintainability; removed redundant code blocks and optimized function calls.

---
+++ ThiSmartUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ThiSmartUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
